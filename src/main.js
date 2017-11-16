91f09fc7797e147eba285dba42e897c6b5929024
戈志刚 <gezhigang1005@163.com>
2017-11-16 18:17:01 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,29 @@
+Vue.component('my-component',{
+    template: '#myComponent',
+    data: function(){
+        return {
+            list: []
+        }
+    },
+    methods: {
+        parseData: function(datas){
+            this.list = datas.data.pageList;
+        }
+    },
+    mounted: function() {
+        let _this = this;
+        // axios.get('/server/getTaobao')
+        // .then(function (res) {
+        //     if(res.status == 200){
+        //         _this.parseData(JSON.parse(res.data));
+        //     }
+        // }).catch(function (err) {
+        //     console.log(error);
+        // });
+        _this.parseData(MYDATALIST);
+    }
+})
+
+var app = new Vue({
+    el: '#app'
+})
